339a97b126f573247a24291377c2fd05bbf77958
aollivierre <abdullahollivierre@gmail.com>
2024-08-26 12:22:52 -0700

    improved logging and removing existing files

--- Public/Test-ProvisioningPack.ps1
+++ Public/Test-ProvisioningPack.ps1
@@ -7,20 +7,32 @@ function Test-ProvisioningPack {
 
     Begin {
         Write-EnhancedLog -Message "Starting Test-ProvisioningPack function" -Level "INFO"
-        Log-Params -Params @{'PPKGName' = $PPKGName}
+        Log-Params -Params $PSCmdlet.MyInvocation.BoundParameters
     }
 
     Process {
         try {
-            Write-EnhancedLog -Message "Testing to see if provisioning package previously installed" -Level "INFO"
-            $PPKGStatus = Get-ProvisioningPackage | Where-Object { $_.PackagePath -like "*$PPKGName*" }
+            Write-EnhancedLog -Message "Checking if the provisioning package '$PPKGName' has been previously installed." -Level "INFO"
+
+            # Retrieve the status of the provisioning package
+            $PPKGStatus = Get-ProvisioningPackage -AllInstalledPackages | Where-Object { $_.PackagePath -like "*$PPKGName*" }
+
             if ($PPKGStatus) {
-                Write-EnhancedLog -Message "Provisioning package previously installed. Removing PPKG." -Level "INFO"
+                Write-EnhancedLog -Message "Provisioning package '$PPKGName' found to be previously installed." -Level "INFO"
+                Write-EnhancedLog -Message "Removing previously installed provisioning package '$PPKGName'." -Level "INFO"
+                
+                # Extract the Package ID
                 $PPKGID = $PPKGStatus.PackageID
                 Remove-ProvisioningPackage -PackageId $PPKGID
+
+                Write-EnhancedLog -Message "Successfully removed provisioning package with ID '$PPKGID'." -Level "INFO"
             }
-        } catch {
-            Write-EnhancedLog -Message "An error occurred while testing provisioning pack: $($_.Exception.Message)" -Level "ERROR"
+            else {
+                Write-EnhancedLog -Message "No previous installation found for provisioning package '$PPKGName'." -Level "INFO"
+            }
+        }
+        catch {
+            Write-EnhancedLog -Message "An error occurred while testing provisioning package '$PPKGName': $($_.Exception.Message)" -Level "ERROR"
             Handle-Error -ErrorRecord $_
         }
     }
@@ -29,10 +41,3 @@ function Test-ProvisioningPack {
         Write-EnhancedLog -Message "Exiting Test-ProvisioningPack function" -Level "INFO"
     }
 }
-
-# # Example usage with splatting
-# $TestProvisioningPackParams = @{
-#     PPKGName = "YourProvisioningPackName"
-# }
-
-# Test-ProvisioningPack @TestProvisioningPackParams
